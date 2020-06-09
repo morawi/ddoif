de3447a02fef2b79be4794ef5c40d91440b016d1
Al-Rawi <ms.alrawi@gmail.com>
2020-06-09 15:41:54 +0100

    Added comments to functions

--- ddoif_io.py
+++ ddoif_io.py
@@ -19,11 +19,45 @@ import numpy as np
 
 
 def dict_to_binary(the_dict):
+    """
+    A function used to convert a python dictionary to a byte string
+
+    ...
+
+    input arguments
+    ----------
+    the_dict: python dictionary
+        
+    output arguments
+    dict_in_bytes : python dictionary as a byte string
+        
+    Methods
+    -------
+    byte_dict = dict_to_binary(in_dict)
+       returns dictionary as byte string
+    """
     dict_in_bytes = json.dumps(the_dict).encode('utf-8')
     num_of_bytes= len(dict_in_bytes) # seems this is important step to remove the tuple!
     return dict_in_bytes, num_of_bytes
 
 def binary_to_dict(dict_in_binary):
+    """
+    A function used to convert a python bytes to a dictionary
+
+    ...
+
+    input arguments
+    ----------
+    dict_in_binary: python dictionary as bytes
+        
+    output arguments
+    the_dict : python dictionary 
+        
+    Methods
+    -------
+    the_dict =binary_to_dict(dict_in_binary)
+       returns dictionary
+    """
     the_dict = json.loads(dict_in_binary.decode('utf-8'))
     return the_dict
 
@@ -31,11 +65,33 @@ def string_to_8_bytes(s):
     s8 = "_"*(8-len(s)) + s
     s8 = bytes(s8, 'utf-8', 'big')
     return s8
+
 def remove_space_from_string(s):
     s= s.replace('_', '') 
     return s
 
 def ddoif_write(ddoif_dict, media_buffer='', out_f='ATest.ddof'):
+    """
+    A function used to write fashion textual item and the relevant mdedia as a ddof file
+
+    ...
+
+    Input arguments
+    ----------
+    - ddoif_dict: dictionary containing textual description of clothing attributes according
+    to ddoif dictionary
+    - media_buffer: a list of media buffer of the clothing item
+    - out_f: the name of output file that has .ddof extension
+        
+    Output arguments:
+    
+        
+    Methods
+    -------
+    ddoif_write(ddoif_dict, media_buffer=media_buf, out_f='ATest.ddof')
+       
+    """
+    
     dict_in_bytes, num_bytes = dict_to_binary(ddoif_dict)
     with open(out_f, 'wb') as file:   
         ddoif_header = b"\x89" + "DDOIF\r\n\x1A\n".encode('ascii') # 10 bytes signature / header
@@ -65,6 +121,27 @@ def ddoif_write(ddoif_dict, media_buffer='', out_f='ATest.ddof'):
         
 
 def ddoif_read(in_f='ATest.ddof', check_CRC=False):
+    """
+    A function used to read fashion textual item and the relevant mdedia stored as a ddof file
+
+    ...
+
+    Input arguments
+    ----------
+    - in_f: the name of output file that has .ddof extension
+    - check_CRC: A flag (True/False) whether to check the file integrity via CRC code
+    
+        
+    Output arguments:
+        - ddoif_dict: Textual attributes of the clothing item according to ddoif dictionayr
+        - media_buffer: a list of media buffer of the clothing item
+    
+        
+    Methods
+    -------
+        ddoif_dict = ddoif_read(in_f='ATest.ddof', check_CRC=True)
+       
+    """
     with open(in_f, 'rb') as file:
         media_buffer={}; media_buffer['buffer'] = []; media_buffer['media_format'] = []  
         reserved_bytes_for_futuer = 16  
